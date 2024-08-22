1fd7691ec7cd3a043f8f6e7126ed861e2293ce4b
Mango <mangodev4@gmail.com>
2024-08-22 18:20:49 +0900

    Initial Commit

---
+++ ManduApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ManduApp
+//
+//  Created by Yujin Son on 8/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ManduApp/ManduAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManduAppApp.swift
+//  ManduApp
+//
+//  Created by Yujin Son on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ManduAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
