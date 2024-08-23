fb44d2f36a730973651b47992845b5d298a6d99e
Mango <mangodev4@gmail.com>
2024-08-23 10:02:05 +0900

    feat. 캘린더뷰 파일 생성

---
+++ ManduApp/View/CalenderView/MDCalenderView.swift
@@ -0,0 +1,18 @@
+//
+//  MDCalenderView.swift
+//  ManduApp
+//
+//  Created by Yujin Son on 8/23/24.
+//
+
+import SwiftUI
+
+struct MDCalenderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MDCalenderView()
+}
