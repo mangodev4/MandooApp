6f53577b786108b7bbc290d533482b795e3f6e74
Mango <mangodev4@gmail.com>
2024-08-26 10:40:27 +0900

    feat. 뷰 생성 및 폴더링
    기획에서 결정된 뷰 파일 미리 생성

---
+++ ManduApp/Extension/ColorExtension.swift
@@ -0,0 +1,18 @@
+//
+//  ColorExtension.swift
+//  ManduApp
+//
+//  Created by Yujin Son on 8/26/24.
+//
+
+import SwiftUI
+
+struct ColorExtension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorExtension()
+}
