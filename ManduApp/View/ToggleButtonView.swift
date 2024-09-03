f19a0b1522b07bfcb63f8a1d42cb23eccfe119bc
Mango <mangodev4@gmail.com>
2024-09-03 11:11:35 +0900

    feat. 토글 버튼 뷰 분리 생성 구현
    여러 개의 버튼 상태 관리하기 위해 뷰 생성

---
+++ ManduApp/View/ToggleButtonView.swift
@@ -0,0 +1,19 @@
+//
+//  ToggleButtonView.swift
+//  ManduApp
+//
+//  Created by Yujin Son on 9/3/24.
+//
+
+import SwiftUI
+import Combine
+
+struct ToggleButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToggleButtonView()
+}
