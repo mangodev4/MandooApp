24c8d735bbe396b56dc689a79891f074f861c76d
Mango <mangodev4@gmail.com>
2024-09-03 17:48:41 +0900

    feat. CalendarViewModel 생성 구현
    Combine적용을 위해 뷰 모델 생성

--- ManduApp/View/CalenderView/CalendarView.swift
+++ ManduApp/View/CalenderView/CalendarView.swift
@@ -6,6 +6,47 @@
 //
 
 import SwiftUI
+import Combine
+
+class CalendarViewModel: ObservableObject {
+    @Published var month: Date
+    
+    init(month: Date) {
+        self.month = month
+    }
+    
+    /// 월 변경
+    func changeMonth(by value: Int) {
+      let calendar = Calendar.current
+      if let newMonth = calendar.date(byAdding: .month, value: value, to: month) {
+        self.month = newMonth
+      }
+    }
+
+    /// 해당 월의 시작 날짜
+    func startOfMonth() -> Date {
+      let components = Calendar.current.dateComponents([.year, .month], from: month)
+      return Calendar.current.date(from: components)!
+    }
+
+    /// 특정 해당 날짜
+    private func getDate(for day: Int) -> Date {
+      return Calendar.current.date(byAdding: .day, value: day, to: startOfMonth())!
+    }
+
+    /// 해당 월에 존재하는 일자 수
+    func numberOfDays(in date: Date) -> Int {
+      return Calendar.current.range(of: .day, in: .month, for: date)?.count ?? 0
+    }
+    
+    /// 해당 월의 첫 날짜가 갖는 해당 주의 몇번째 요일
+    func firstWeekdayOfMonth(in date: Date) -> Int {
+      let components = Calendar.current.dateComponents([.year, .month], from: date)
+      let firstDayOfMonth = Calendar.current.date(from: components)!
+      
+      return Calendar.current.component(.weekday, from: firstDayOfMonth)
+    }
+}
 
 struct CalenderView: View {
     @State var month: Date
@@ -239,37 +280,8 @@ struct CalenderView: View {
 }
 // MARK: - 내부 메서드
 private extension CalenderView {
-  /// 특정 해당 날짜
-  private func getDate(for day: Int) -> Date {
-    return Calendar.current.date(byAdding: .day, value: day, to: startOfMonth())!
-  }
-  
-  /// 해당 월의 시작 날짜
-  func startOfMonth() -> Date {
-    let components = Calendar.current.dateComponents([.year, .month], from: month)
-    return Calendar.current.date(from: components)!
-  }
-  
-  /// 해당 월에 존재하는 일자 수
-  func numberOfDays(in date: Date) -> Int {
-    return Calendar.current.range(of: .day, in: .month, for: date)?.count ?? 0
-  }
-  
-  /// 해당 월의 첫 날짜가 갖는 해당 주의 몇번째 요일
-  func firstWeekdayOfMonth(in date: Date) -> Int {
-    let components = Calendar.current.dateComponents([.year, .month], from: date)
-    let firstDayOfMonth = Calendar.current.date(from: components)!
     
-    return Calendar.current.component(.weekday, from: firstDayOfMonth)
-  }
   
-  /// 월 변경
-  func changeMonth(by value: Int) {
-    let calendar = Calendar.current
-    if let newMonth = calendar.date(byAdding: .month, value: value, to: month) {
-      self.month = newMonth
-    }
-  }
 }
 
 // MARK: - Static 프로퍼티
