c614b3853eb6385e798a71955516c80c5f454950
Mango <mangodev4@gmail.com>
2024-09-03 13:11:04 +0900

    feat. 토글뷰 만두Item별 버튼 구현
    만두 종류별로 선택할 수 있는 토글 버튼 구현

--- ManduApp/View/ToggleButtonView.swift
+++ ManduApp/View/ToggleButtonView.swift
@@ -6,11 +6,37 @@
 //
 
 import SwiftUI
-import Combine
 
 struct ToggleButtonView: View {
+    @State private var selectedIndex: Int? = nil
+    
+    let buttons = ["찐만두", "군만두", "왕만두", "김치만두", "딤섬만두", "교자만두", "기타"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            ForEach(0..<buttons.count, id: \.self) { index in
+                Button (action: {
+                    if selectedIndex == index {
+                        selectedIndex = nil
+                    } else {
+                        selectedIndex = index
+                    }
+                }) {
+                    Text(buttons[index])
+                        .font(.pretendBold14)
+                        .foregroundColor(selectedIndex == index ? Color.white: Color.black )
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.blue, lineWidth: 2)
+                                .fill(selectedIndex == index ? Color.blue : Color.white)
+                        )
+
+
+                }
+
+            }
+        }
     }
 }
 
